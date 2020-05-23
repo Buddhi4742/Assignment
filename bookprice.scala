fb4071ad3dfbc3675ff1735b9c1947800b82c4de
Buddhi <chathuranga4742@gmail.com>
2020-05-23 16:19:18 +0000

    2nd commit

--- bookprice.scala
+++ bookprice.scala
@@ -1,8 +1,14 @@
 object bookprice extends App{
 //assuming a bookstore is making the wholesale order of 60 copies just to clarify the question
-def get_additional(num_of_books:Int)=num_of_books%50;
-var excess:Int=get_additional(60);
-var ship_costs=excess*0.75+3;
-var total_cost:Double=(60*24.95*40)/100 +ship_costs;
-println(total_cost);
+def get_additional(num_of_books:Int)=num_of_books-50;
+def get_ship_costs(excess:Int)=excess*0.75+3;
+
+def get_total_cost(num_of_books:Int):Double={
+var excess:Int=get_additional(num_of_books);
+var ship_costs:Double=get_ship_costs(excess);
+var total_cost:Double=ship_costs+num_of_books*24.95*0.4;
+return total_cost;
+}
+
+println(get_total_cost(60));
 }
